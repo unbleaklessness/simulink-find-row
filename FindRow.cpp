e1e89e480e0c9643b689620f8251d69401d48a5b
Sorokin Oleg <sorokin.oleg.edu@gmail.com>
2020-12-12 16:13:40 +0400

    Remove caching and STD dependency

--- FindRow.cpp
+++ FindRow.cpp
@@ -1,15 +1,15 @@
 #define S_FUNCTION_NAME FindRow
 #define S_FUNCTION_LEVEL 2
 
-#define IX(row, column) (row + column * port0Dimensions[0])
-
-#include <vector>
-#include <algorithm>
-
 #include "simstruc.h"
 
 #define MDL_SET_INPUT_PORT_DIMENSION_INFO
 static void mdlSetInputPortDimensionInfo(SimStruct *S, int_T port, const DimsInfo_T *info) {
+
+	if (info->numDims != 2) {
+		ssSetErrorStatus(S, "FindRow: Error: Inputs must have a matrix dimensions.");
+	}
+
 	ssSetInputPortMatrixDimensions(S, port, info->dims[0], info->dims[1]);
 }
 
@@ -51,54 +51,56 @@ static void mdlInitializeSampleTimes(SimStruct *S) {
 static void mdlOutputs(SimStruct *S, int_T id) {}
 
 #define MDL_START
-static void mdlStart(SimStruct *S)
-{
+static void mdlStart(SimStruct *S) {
+
 	int_T *port0Dimensions = ssGetInputPortDimensions(S, 0);
 	int_T *port1Dimensions = ssGetInputPortDimensions(S, 1);
 
-	if (port0Dimensions[0] < 2 || 
-		port1Dimensions[0] != 1 ||
-		port1Dimensions[1] != port0Dimensions[1])
-	{
-		ssSetErrorStatus(S, "FindRow: Error: Input dimensions are incorrect.");
+	if (port1Dimensions[0] != 1) {
+		ssSetErrorStatus(S, "FindRow: Error: Dimensions of input port 2 must be [1, N].");
+	}
+
+	if (port0Dimensions[1] != port1Dimensions[1]) {
+		ssSetErrorStatus(S, "FindRow: Error: Dimensions of input ports 1 and 2 must be [M, N] and [1, N].");
 	}
 }
 
 #define MDL_UPDATE
 static void mdlUpdate(SimStruct *S, int_T tid) {
 
-	static int_T *port0Dimensions = ssGetInputPortDimensions(S, 0);
-	static int_T *port1Dimensions = ssGetInputPortDimensions(S, 1);
-	
-	static std::vector<std::vector<real_T>> vector;
-	
-	static bool cached = false;
+	int_T *port0Dimensions = ssGetInputPortDimensions(S, 0);
+	int_T port0Rows = port0Dimensions[0];
+	int_T port0Columns = port0Dimensions[1];
 
-	if (!cached) {
+	InputRealPtrsType input0 = ssGetInputPortRealSignalPtrs(S, 0);
+	InputRealPtrsType input1 = ssGetInputPortRealSignalPtrs(S, 1);
 
-		for (size_t i = 0; i < port0Dimensions[0]; i++) {
-			std::vector<real_T> thisVector;
-			for (size_t j = 0; j < port0Dimensions[1]; j++) {
-				thisVector.push_back(*ssGetInputPortRealSignalPtrs(S, 0)[IX(i, j)]);
+	size_t count = port0Rows - 1;
+	size_t first = 0;
+	size_t it;
+	size_t step;
+	bool flag;
+
+	while (count > 0) {
+		it = first;
+		step = count / 2;
+		it += step;
+		flag = false;
+		for (size_t i = 0; i < port0Columns; i++) {
+			if (*input1[i] >= *input0[it + i * port0Rows]) {
+				flag = true;
+				break;
 			}
-			vector.push_back(thisVector);
 		}
-
-		cached = true;
+		if (flag) {
+			first = ++it;
+			count -= step + 1;
+		} else {
+			count = step;
+		}
 	}
 
-	std::vector<real_T> thisVector;
-	for (size_t i = 0; i < port1Dimensions[1]; i++) {
-		thisVector.push_back(*ssGetInputPortRealSignalPtrs(S, 1)[i]);
-	}
-
-	auto it = std::upper_bound(vector.begin(), vector.end(), thisVector);
-
-	if (it == vector.end()) {
-		std::advance(it, -1);
-	}
-
-	*ssGetOutputPortRealSignal(S, 0) = it - vector.begin();
+	*ssGetOutputPortRealSignal(S, 0) = first;
 }
 
 static void mdlTerminate(SimStruct *S) {}
