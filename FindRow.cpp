a7338ed3ee84b2871516eeb9728ee04697add102
Sorokin Oleg <sorokin.oleg.edu@gmail.com>
2021-02-06 16:29:20 +0400

    Instant find (full rework)

--- FindRow.cpp
+++ FindRow.cpp
@@ -3,6 +3,10 @@
 
 #include "simstruc.h"
 
+#include <algorithm>
+#include <vector>
+#include <cstdint>
+
 #define MDL_SET_INPUT_PORT_DIMENSION_INFO
 static void mdlSetInputPortDimensionInfo(SimStruct *S, int_T port, const DimsInfo_T *info) {
 
@@ -18,7 +22,14 @@ static void mdlSetOutputPortDimensionInfo(SimStruct *S, int_T port, const DimsIn
 
 static void mdlInitializeSizes(SimStruct *S) {
 
-	if (!ssSetNumInputPorts(S, 2)) {
+	// Inputs:
+	// - Deltas [1, N].
+	// - Minimums (first row), maximums (second row) [2, N].
+	// - Numbers of elements [1, N].
+	// - Input [1, N].
+	// Ouput is an index of input.
+
+	if (!ssSetNumInputPorts(S, 4)) {
 		return;
 	}
 
@@ -29,6 +40,16 @@ static void mdlInitializeSizes(SimStruct *S) {
 
 	ssSetInputPortDirectFeedThrough(S, 1, 1);
 	if(!ssSetInputPortDimensionInfo(S, 1, DYNAMIC_DIMENSION)) {
+		return;
+	}
+
+	ssSetInputPortDirectFeedThrough(S, 2, 1);
+	if(!ssSetInputPortDimensionInfo(S, 2, DYNAMIC_DIMENSION)) {
+		return;
+	}
+
+	ssSetInputPortDirectFeedThrough(S, 3, 1);
+	if(!ssSetInputPortDimensionInfo(S, 3, DYNAMIC_DIMENSION)) {
 		return;
 	}
 	
@@ -55,52 +76,53 @@ static void mdlStart(SimStruct *S) {
 
 	int_T *port0Dimensions = ssGetInputPortDimensions(S, 0);
 	int_T *port1Dimensions = ssGetInputPortDimensions(S, 1);
+	int_T *port2Dimensions = ssGetInputPortDimensions(S, 2);
+	int_T *port3Dimensions = ssGetInputPortDimensions(S, 3);
 
-	if (port1Dimensions[0] != 1) {
-		ssSetErrorStatus(S, "FindRow: Error: Dimensions of input port 2 must be [1, N].");
+	if (port0Dimensions[0] != 1) {
+		ssSetErrorStatus(S, "FindRow: Error: Dimensions of input port 1 must be [1, N].");
 	}
 
-	if (port0Dimensions[1] != port1Dimensions[1]) {
-		ssSetErrorStatus(S, "FindRow: Error: Dimensions of input ports 1 and 2 must be [M, N] and [1, N].");
+	if (port1Dimensions[0] != 2 || port0Dimensions[1] != port1Dimensions[1]) {
+		ssSetErrorStatus(S, "FindRow: Error: Dimensions of input port 1 and 2 must be [1, N] and [2, N].");
+	}
+
+	if (port2Dimensions[0] != 1 || port0Dimensions[1] != port2Dimensions[1]) {
+		ssSetErrorStatus(S, "FindRow: Error: Dimensions of input port 1 and 3 must be [1, N] and [1, N].");
+	}
+
+	if (port3Dimensions[0] != 1 || port0Dimensions[1] != port3Dimensions[1]) {
+		ssSetErrorStatus(S, "FindRow: Error: Dimensions of input port 1 and 4 must be [1, N] and [1, N].");
 	}
 }
 
 #define MDL_UPDATE
 static void mdlUpdate(SimStruct *S, int_T tid) {
 
-	int_T *port0Dimensions = ssGetInputPortDimensions(S, 0);
-	int_T port0Rows = port0Dimensions[0];
-	int_T port0Columns = port0Dimensions[1];
+	int_T *port1Dimensions = ssGetInputPortDimensions(S, 1);
 
 	InputRealPtrsType input0 = ssGetInputPortRealSignalPtrs(S, 0);
 	InputRealPtrsType input1 = ssGetInputPortRealSignalPtrs(S, 1);
+	InputRealPtrsType input2 = ssGetInputPortRealSignalPtrs(S, 2);
+	InputRealPtrsType input3 = ssGetInputPortRealSignalPtrs(S, 3);
 
-	size_t count = port0Rows - 1;
-	size_t first = 0;
-	size_t it;
-	size_t step;
-	bool flag;
+	std::vector<real_T> temporary;
 
-	while (count > 0) {
-		it = first;
-		step = count / 2;
-		it += step;
-		flag = false;
-		for (size_t i = 0; i < port0Columns; i++) {
-			if (*input1[i] >= *input0[it + i * port0Rows]) {
-				flag = true;
-				break;
-			}
-		}
-		if (flag) {
-			first = ++it;
-			count -= step + 1;
-		} else {
-			count = step;
-		}
+	for (size_t i = 0; i < port1Dimensions[1]; i++) {
+		real_T minimum = std::round(*input1[0 + i * port1Dimensions[0]] / *input0[i]);
+		real_T maximum = std::round(*input1[1 + i * port1Dimensions[0]] / *input0[i]);
+		temporary.push_back(std::max(std::min(std::round(*input3[i] / *input0[i]), maximum), minimum) - minimum);
 	}
 
-	*ssGetOutputPortRealSignal(S, 0) = first;
+	real_T index = 0;
+	real_T lengths = 1;
+
+	for (int64_t i = port1Dimensions[1] - 1; i >= 0; i--) {
+		index += temporary[i] * lengths;
+		lengths *= *input2[i];
+	}
+
+	*ssGetOutputPortRealSignal(S, 0) = index;
 }
 
 static void mdlTerminate(SimStruct *S) {}
